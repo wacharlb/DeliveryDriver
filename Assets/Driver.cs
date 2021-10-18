cafcd6d540792c57fa0a403bcb617906df2f0980
Charles Beall <wacharlb@gmail.com>
2021-10-18 16:55:06 -0700

    This commit contains the following changes: - Added driver box collider - Added circle collider - Added collision script - Added rigid body component to driver and circle object

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float steerSpeed = 300f;
+    [SerializeField]
+    private float moveSpeed = 20f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
